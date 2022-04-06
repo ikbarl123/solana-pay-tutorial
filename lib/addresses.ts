6ab819ca97405b3d39fa30efbe0fb2fe86cd1bb8
ikbarl123 <ikbarl123@gmail.com>
2022-04-06 12:35:21 +0700

    Public Key Address

---
+++ lib/addresses.ts
@@ -0,0 +1,7 @@
+import { PublicKey } from "@solana/web3.js"
+
+// Your shop wallet address
+export const shopAddress = new PublicKey('2E513h7mmPve7DY8xFHdsYvYnbNfUdmFqLiko74VaESB') 
+//usdc
+export const usdcAddress = new PublicKey('Gh9ZwEmdLJ8DscKNTkTqPbNwLNNBjuSzaG9Vp2KGtKJr')
+export const couponAddress = new PublicKey('GCc6ruYzGMbMNaJ8bQTWBbs1p49iP5NjNFtnacLBpEL1')
